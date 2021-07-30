00e9b0d83feee45ffbd5577bd66f7450dfb31d61
Tr1sk33l <mastermind51@gmail.com>
2021-07-29 22:05:17 -0300

    format
    format for 2 decimal

--- tipcalculator.py
+++ tipcalculator.py
@@ -12,5 +12,5 @@ total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
-
+final_amount = "{:.2f}".format(bill_per_person)
 print(f"Each person should pay: ${final_amount}")
